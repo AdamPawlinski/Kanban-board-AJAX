a38798056a1f63983629fe371d49c780aed6144e
Adam Pawliński <adam.pawlinski@gmail.com>
2019-01-16 11:14:44 +0100

    css style changed, dropdown menu added, sorting columns enabled

--- js/Board.js
+++ js/Board.js
@@ -7,7 +7,7 @@ var board = {
 	element: $('#board .column-container')
 };
 
-$('.create-column').on('click', function() {
+$('#create-column').on('click', function() {
 	var columnName = prompt('Enter a column name');
 	if (columnName === null) {
 		return
@@ -25,9 +25,25 @@ $('.create-column').on('click', function() {
 	});
 });
 
+// dropdown menu
+
+$('.dropdown-nav').mouseenter(function(){
+	$(this).addClass('show');
+	$('.dropdown-container').css('display', 'block');
+	console.dir(this);
+});
+$('.dropdown-container').mouseleave(function(){
+	$(this).removeClass('show');
+	$('.dropdown-container').css('display', 'none');
+});
+
 function initSortable() {
-    $('.card-list').sortable({
-      connectWith: '.card-list',
-      placeholder: 'card-placeholder'
-    }).disableSelection();
+    $('.column-card-list').sortable({
+      connectWith: '.column-card-list',
+      placeholder: '.card-placeholder'
+		}).disableSelection();
+		$('.column-container').sortable({
+      connectWith: '.column-container',
+      placeholder: '.card-placeholder'
+		}).disableSelection();
   }
